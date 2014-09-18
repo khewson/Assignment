bb35e07804d02b10156db1bb38e4c78376c531f5
44422 <44422@longroad.ac.uk>
2014-09-18 14:59:35 +0100

    Think it's finished could do with some work

---
+++ development exercises/Number 1.py
@@ -0,0 +1,10 @@
+#Kim Hewson
+#18/09/14
+#Development exercise one
+
+num1=int(input("Enter a number"))
+num2=int(input("Enter a second number"))
+div=num1//num2
+mod=num1%num2
+print(div)
+print(mod)
