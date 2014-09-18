e6abb4743a86c8a6ed2be85af7c20742dfa7f54f
44422 <44422@longroad.ac.uk>
2014-09-18 15:20:59 +0100

    Finished and works

---
+++ development exercises/Number 3.py
@@ -0,0 +1,11 @@
+#Kim Hewson
+#18/09/14
+#Development exersise 3
+
+
+height_inches=int(input("Enter your height in inches only"))
+height_cm=(height_inches*2.54)
+weight_stone=int(input("Enter your weight in stone"))
+weight_kg=(weight_stone*6.364)
+print("Your height is {0}cm".format(height_cm))
+print("Your weight in kg is {0}kg".format(weight_kg))
