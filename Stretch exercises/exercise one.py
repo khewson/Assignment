47e2ae914070f1fbcdbd976028d17e51acd21244
44422 <44422@longroad.ac.uk>
2014-09-18 15:57:07 +0100

    Final one

---
+++ Stretch exercises/exercise one.py
@@ -0,0 +1,10 @@
+#Kim Hewson
+#18/09/14
+#Stretch exercise one
+
+
+
+length=int(input("Enter length (m): "))
+width=int(input("Enter width (m): "))
+cost=((length-2)*(width-2))*10
+print("The cost to turf your garden will be £{0}".format(cost))
